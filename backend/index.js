586b5ee6bf35f2a3685604c54799a114deda710f
jakkritsena1 <jakkritsena1@gmail.com>
2025-06-23 23:38:23 +0700

    Cors แบบเขียนเอง

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,21 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     res.setHeader(
+//         "Access-Control-Allow-Origin",
+//         "*"
+//     );
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, PUT, DELETE, OPTIONS"
+//     );
+//     next();
+// })
 app.use(express.json());
 app.use(cors());
 app.get('/customers/', async (req, res) => {
