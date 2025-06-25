d53eb359f9c71c751e0d86903e8438dc9bb8a9a1
jakkritsena1 <jakkritsena1@gmail.com>
2025-06-25 17:16:04 +0700

    refresh หน้าหลังจาก เพิ่ม แก้ไข ลบ

--- frontend/src/components/table.jsx
+++ frontend/src/components/table.jsx
@@ -29,72 +29,74 @@ export default function Table() {
     }
     const Push = async (e) => {
         e.preventDefault();
-        try {
-            setNewRow({ name: '', lastname: '', address: '', telephone: '' });
-            const res = await postData(newRow.name, newRow.lastname, newRow.address, newRow.telephone);
-            console.log('push success', res.status)
-            fetchData();
-        } catch (err) {
-            console.error('push fail: ', err);
+        if (newRow.name.trim() && newRow.lastname.trim() && newRow.address.trim() && newRow.telephone.trim()) {
+            try {
+                setNewRow({ name: '', lastname: '', address: '', telephone: '' });
+                const res = await postData(newRow.name, newRow.lastname, newRow.address, newRow.telephone);
+                console.log('push success', res.status)
+                fetchData();
+            } catch (err) {
+                console.error('push fail: ', err);
+            }
+        } else { console.log('invalid data') }
+        };
+        const Update = async (e, index) => {
+            e.preventDefault();
+            try {
+                const row = rows[index];
+                const res = await pushData(row.id, row.name, row.lastname, row.address, row.telephone);
+                console.log('update success', res.status)
+                fetchData();
+            } catch (err) {
+                console.error('update fail:', err);
+            }
+        };
+        const Delete = async (e, index) => {
+            e.preventDefault();
+            try {
+                const row = rows[index];
+                const res = await deleteData(row.id);
+                console.log('delete success', res.status)
+                fetchData();
+            } catch (err) {
+                console.error('delete fail', err)
+            }
         }
-    };
-    const Update = async (e, index) => {
-        e.preventDefault();
-        try {
-            const row = rows[index];
-            const res = await pushData(row.id, row.name, row.lastname, row.address, row.telephone);
-            console.log('update success', res.status)
-            fetchData();
-        } catch (err) {
-            console.error('update fail:', err);
-        }
-    };
-    const Delete = async (e, index) => {
-        e.preventDefault();
-        try {
-            const row = rows[index];
-            const res = await deleteData(row.id);
-            console.log('delete success', res.status)
-            fetchData();
-        } catch (err) {
-            console.error('delete fail', err)
-        }
-    }
 
-    return (
-        <>
-            <div>
-                <table className="table-content">
-                    <thead>
-                        <tr>
-                            <th>data1</th>
-                            <th>data2</th>
-                            <th>data3</th>
-                            <th>data4</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {rows.map((item, index) => (
-                            <tr className='data' key={item.id}>
-                                <td><input type='text' placeholder='name' name='name' value={item.name} onChange={e => changeData(index, 'name', e.target.value)} required /></td>
-                                <td><input type='text' placeholder='lastname' name='lastname' value={item.lastname} onChange={e => changeData(index, 'lastname', e.target.value)} required /></td>
-                                <td><input type='text' placeholder='address' name='address' value={item.address} onChange={e => changeData(index, 'address', e.target.value)} required /></td>
-                                <td><input type='text' placeholder='telephone' name='telephone' value={item.telephone} onChange={e => changeData(index, 'telephone', e.target.value)} required /></td>
-                                <td className='btn-submit'><button type='button' onClick={(e) => Update(e, index)}>EDIT</button></td>
-                                <td className='btn-submit'><button type='button' onClick={(e) => Push(e)}>PUSH</button></td>
-                                <td className='btn-submit'><button type='button' onClick={(e) => Delete(e, index)}>DELETE</button></td>
+        return (
+            <>
+                <div>
+                    <table className="table-content">
+                        <thead>
+                            <tr>
+                                <th>data1</th>
+                                <th>data2</th>
+                                <th>data3</th>
+                                <th>data4</th>
                             </tr>
-                        ))}
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td className='btn-addRow'><button type='submit' onClick={(e) => addRow(e)}>add rows</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
-}
+                        </thead>
+                        <tbody>
+                            {rows.map((item, index) => (
+                                <tr className='data' key={item.id}>
+                                    <td><input type='text' placeholder='name' name='name' value={item.name} onChange={e => changeData(index, 'name', e.target.value)} required /></td>
+                                    <td><input type='text' placeholder='lastname' name='lastname' value={item.lastname} onChange={e => changeData(index, 'lastname', e.target.value)} required /></td>
+                                    <td><input type='text' placeholder='address' name='address' value={item.address} onChange={e => changeData(index, 'address', e.target.value)} required /></td>
+                                    <td><input type='text' placeholder='telephone' name='telephone' value={item.telephone} onChange={e => changeData(index, 'telephone', e.target.value)} required /></td>
+                                    <td className='btn-submit'><button type='button' onClick={(e) => Update(e, index)}>EDIT</button></td>
+                                    <td className='btn-submit'><button type='button' onClick={(e) => Push(e)}>PUSH</button></td>
+                                    <td className='btn-submit'><button type='button' onClick={(e) => Delete(e, index)}>DELETE</button></td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td className='btn-addRow'><button type='submit' onClick={(e) => addRow(e)}>add rows</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </>
+        )
+    }
