e3a1d89b3d8a5689e44a281406c8b58fcb19b1ef
jakkritsena1 <jakkritsena1@gmail.com>
2025-06-25 22:23:43 +0700

    admin api

---
+++ frontend/src/components/table-admin.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import '../assets/css/table.css';
+import { getData, pushData, postData, deleteData } from '../hooks/admin-api.js'
+
+export default function Table() {
+    const [rows, setRows] = useState([]);
+    const [newRow, setNewRow] = useState({
+        name: '',
+        lastname: '',
+        user: '',
+        password: ''
+    });
+    const fetchData = () => {
+        getData()
+            .then(data => setRows(data))
+            .catch(err => console.error('โหลดข้อมูลไม่สำเร็จ', err));
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const changeData = (index, field, value) => {
+        const newRows = [...rows];
+        newRows[index][field] = value;
+        setRows(newRows);
+    };
+    const addRow = (e) => {
+        e.preventDefault();
+        setRows([...rows, newRow]);
+    }
+    const Push = async (e) => {
+        e.preventDefault();
+        if (newRow.name.trim() && newRow.lastname.trim() && newRow.user.trim() && newRow.password.trim()) {
+            try {
+                setNewRow({ name: '', lastname: '', user: '', password: '' });
+                const res = await postData(newRow.name, newRow.lastname, newRow.user, newRow.password);
+                console.log('push success', res.status)
+                fetchData();
+            } catch (err) {
+                console.error('push fail: ', err);
+            }
+        } else { console.log('invalid data') }
+        };
+        const Update = async (e, index) => {
+            e.preventDefault();
+            try {
+                const row = rows[index];
+                const res = await pushData(row.id, row.name, row.lastname, row.user, row.password);
+                console.log('update success', res.status)
+                fetchData();
+            } catch (err) {
+                console.error('update fail:', err);
+            }
+        };
+        const Delete = async (e, index) => {
+            e.preventDefault();
+            try {
+                const row = rows[index];
+                const res = await deleteData(row.id);
+                console.log('delete success', res.status)
+                fetchData();
+            } catch (err) {
+                console.error('delete fail', err)
+            }
+        }
+
+        return (
+            <>
+                <div>
+                    <table className="table-content">
+                        <thead>
+                            <tr>
+                                <th>data1</th>
+                                <th>data2</th>
+                                <th>data3</th>
+                                <th>data4</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rows.map((item, index) => (
+                                <tr className='data' key={item.id}>
+                                    <td><input type='text' placeholder='name' name='name' value={item.name} onChange={e => changeData(index, 'name', e.target.value)} required /></td>
+                                    <td><input type='text' placeholder='lastname' name='lastname' value={item.lastname} onChange={e => changeData(index, 'lastname', e.target.value)} required /></td>
+                                    <td><input type='text' placeholder='user' name='user' value={item.user} onChange={e => changeData(index, 'user', e.target.value)} required /></td>
+                                    <td><input type='text' placeholder='password' name='password' value={item.password} onChange={e => changeData(index, 'password', e.target.value)} required /></td>
+                                    <td className='btn-submit'><button type='button' onClick={(e) => Update(e, index)}>EDIT</button></td>
+                                    <td className='btn-submit'><button type='button' onClick={(e) => Push(e)}>PUSH</button></td>
+                                    <td className='btn-submit'><button type='button' onClick={(e) => Delete(e, index)}>DELETE</button></td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td className='btn-addRow'><button type='submit' onClick={(e) => addRow(e)}>add rows</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </>
+        )
+    }
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/header.jsx';
 import Table from '../components/table.jsx';
 import Table_hr from '../components/table-hr.jsx';
+import Table_admin from '../components/table-admin.jsx';
 
 export default function Dashboard() {
 
@@ -9,7 +10,8 @@ export default function Dashboard() {
         <>
             <Header />
             {/* <Table /> */}
-            <Table_hr />
+            {/* <Table_hr /> */}
+            <Table_admin />
         </>
     )
 }
